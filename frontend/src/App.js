0efc77a509306ba3f56627c339681f1144a71d66
Jason Paciorek <paciorekjason@gmail.com>
2024-10-10 18:55:32 -0500

    Server Complete and starting app

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import { useEffect, useState } from "react";
+
+function App() {
+    const [numbers, setNumbers] = useState([]);
+
+    useEffect(() => {}, []);
+
+    return null;
+}
+
+export default App;
